84f06682851b743a5405814bb57412cb7152a2b1
Neil Dick <neiljdick@gmail.com>
2020-09-19 09:49:30 +1000

    Working Condition Variables

--- kern/thread/synch.c
+++ kern/thread/synch.c
@@ -276,7 +276,13 @@ cv_create(const char *name)
 		return NULL;
 	}
 
-	// add stuff here as needed
+	cv->cv_wchan = wchan_create(cv->cv_name);
+	if (cv->cv_wchan == NULL) {
+		kfree(cv->cv_name);
+		kfree(cv);
+	}
+
+	spinlock_init(&cv->cv_slock);
 
 	return cv;
 }
@@ -286,8 +292,8 @@ cv_destroy(struct cv *cv)
 {
 	KASSERT(cv != NULL);
 
-	// add stuff here as needed
-
+	spinlock_cleanup(&cv->cv_slock);
+	wchan_destroy(cv->cv_wchan);
 	kfree(cv->cv_name);
 	kfree(cv);
 }
@@ -295,23 +301,41 @@ cv_destroy(struct cv *cv)
 void
 cv_wait(struct cv *cv, struct lock *lock)
 {
-	// Write this
-	(void)cv;    // suppress warning until code gets written
-	(void)lock;  // suppress warning until code gets written
+	KASSERT(cv != NULL);
+	KASSERT(lock != NULL);
+
+	KASSERT(lock_do_i_hold(lock) == true);
+
+	spinlock_acquire(&cv->cv_slock);
+	lock_release(lock);
+
+	wchan_sleep(cv->cv_wchan, &cv->cv_slock);
+	spinlock_release(&cv->cv_slock);
+	lock_acquire(lock);
+
+
 }
 
 void
 cv_signal(struct cv *cv, struct lock *lock)
 {
-	// Write this
-	(void)cv;    // suppress warning until code gets written
-	(void)lock;  // suppress warning until code gets written
+	KASSERT(cv != NULL);
+	KASSERT(lock != NULL);
+	KASSERT(lock_do_i_hold(lock) == true);
+
+	spinlock_acquire(&cv->cv_slock);
+	wchan_wakeone(cv->cv_wchan, &cv->cv_slock);
+	spinlock_release(&cv->cv_slock);
 }
 
 void
 cv_broadcast(struct cv *cv, struct lock *lock)
 {
-	// Write this
-	(void)cv;    // suppress warning until code gets written
-	(void)lock;  // suppress warning until code gets written
+	KASSERT(cv != NULL);
+	KASSERT(lock != NULL);
+	KASSERT(lock_do_i_hold(lock) == true);
+
+	spinlock_acquire(&cv->cv_slock);
+	wchan_wakeall(cv->cv_wchan, &cv->cv_slock);
+	spinlock_release(&cv->cv_slock);
 }
