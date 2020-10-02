fec290c788a3ea5fed181617a9bc4a61de33f4f2
Neil Dick <neiljdick@gmail.com>
2020-10-03 07:44:55 +1000

    Add test for RW Locks
    Simple test for RW locks. It starts a number of reader threads
    and a single writer thread. They compete to access a variable,
    it passes if all the writes complete.
    
    Not exhaustive.

--- kern/test/rwtest.c
+++ kern/test/rwtest.c
@@ -16,12 +16,96 @@
  * Use these stubs to test your reader-writer locks.
  */
 
-int rwtest(int nargs, char **args) {
+#define CREATELOOPS 10
+#define NTHREADS 5
+#define LOOPS 1000
+
+static struct rwlock *testlock;
+static struct cv *sync;
+static struct lock *sync_lock;
+static int test_status = TEST161_FAIL;
+static volatile int prot_val = 0;
+volatile int done = 0;
+
+static void
+rwtestthread(void *junk, unsigned long i)
+{
+	(void)junk;
+	(void)i;
+	int local = 0;
+	done++;
+	lock_acquire(sync_lock);
+	cv_broadcast(sync, sync_lock);
+	lock_release(sync_lock);
+	for (i = 0; i < LOOPS; i++) {
+		random_yielder(5);
+		rwlock_acquire_read(testlock);
+		local = prot_val;
+		kprintf("read %d\n", local);
+		rwlock_release_read(testlock);
+	}
+	lock_acquire(sync_lock);
+	done--;
+	cv_broadcast(sync, sync_lock);
+	lock_release(sync_lock);
+}
+
+
+
+int rwtest(int nargs, char **args)
+{
 	(void)nargs;
 	(void)args;
 
-	kprintf_n("rwt1 unimplemented\n");
-	success(TEST161_FAIL, SECRET, "rwt1");
+	int i, result;
+	kprintf_n("Starting rwt1...\n");
+
+	sync = cv_create("sync");
+	KASSERT(sync);
+	sync_lock = lock_create("sync_lock");
+	KASSERT(sync);
+
+	for (i=0; i<CREATELOOPS; i++) {
+		kprintf_t(".");
+		testlock = rwlock_create("testlock");
+		if (testlock == NULL) {
+			panic("rwlock1: rwlock_create failed\n");
+		}
+		if (i != CREATELOOPS - 1) {
+			rwlock_destroy(testlock);
+		}
+	}
+
+	lock_acquire(sync_lock);
+	for (i=0; i<NTHREADS; i++) {
+		kprintf_t(".");
+		result = thread_fork("rwtest", NULL, rwtestthread, NULL, i);
+		if (result) {
+			panic("rw1: thread_fork failed: %s\n",
+			      strerror(result));
+		}
+	}
+	cv_wait(sync, sync_lock);
+	lock_release(sync_lock);
+	for (int i = 0; i < LOOPS; i++) {
+		random_yielder(100);
+		rwlock_acquire_write(testlock);
+		prot_val = i;
+		rwlock_release_write(testlock);
+	}
+	kprintf_n("Writer threads finished\n");
+
+	lock_acquire(sync_lock);
+	while (done != 0)
+		cv_wait(sync, sync_lock);
+	lock_release(sync_lock);
+
+	rwlock_destroy(testlock);
+	testlock = NULL;
+	if (prot_val == LOOPS-1)
+		test_status = TEST161_SUCCESS;
+	kprintf("Final value %d\n", prot_val);
+	success(test_status, SECRET, "rwt1");
 
 	return 0;
 }
